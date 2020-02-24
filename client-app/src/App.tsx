f82997dc11aa62efc26b6dd92d01f379359eafc7
Aris Bartee <arisbartee@gmail.com>
2020-02-23 18:53:24 -0600

    end of section 3

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Header, Icon, List } from "semantic-ui-react";
+import "./App.css";
+import axios from "axios";
 
-
-const App:React.FC = () => {
-  return (
-    <div className="App">
-
-    </div>
-  );
+class App extends Component {
+  state = {
+    values: []
+  };
+  componentDidMount() {
+    axios.get("http://localhost:5000/api/values").then(response => {
+      this.setState({
+        values: response.data
+      });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Header as="h2">
+          <Icon name="users" />
+          <Header.Content>Reactivities</Header.Content>
+        </Header>
+        <List bulleted>
+          {this.state.values.map((value: any) => (
+            <List.Item key={value.id}>{value.name}</List.Item>
+          ))}
+        </List>
+      </div>
+    );
+  }
 }
 
 export default App;
